d1e226c583560d1f85d55a5ea9433de628ad352b
Christoph Dobraunig <christoph.dobraunig@iaik.tugraz.at>
2014-07-15 17:26:21 +0200

    bugfix off by one error

--- tool/ascon.cpp
+++ tool/ascon.cpp
@@ -25,7 +25,7 @@ std::vector<UpdatePos> AsconState::diff(const StateMask& other) {
   std::vector<UpdatePos> result;
   for (int i = 0; i < 5; ++i) {
     diffword = (words[i].caremask.canbe1 ^ other[i].caremask.canbe1) | (words[i].caremask.care ^ other[i].caremask.care);
-    for (int b = 0; b < 63 && diffword; ++b) {
+    for (int b = 0; b < 64 && diffword; ++b) {
       if (diffword & 1)
         result.emplace_back(0, i, b, 0);
       diffword >>= 1;
